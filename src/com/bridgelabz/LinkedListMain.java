9a8cbd009df0bb48f1010f68d8285e2ac5dc443b
drisyaathul <drisyak809@gmail.com>
2023-04-28 01:02:28 +0800

    Added: Insert 30 between 56 and 70

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,7 +4,7 @@ public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to LinkedList");
         /*
-         UC2: Simple Linked list by pushing 70,30,56.
+         UC2: Linked list by pushing 70,30,56.
          */
         LinkedList<Integer> linkedList1 = new LinkedList<>();
         linkedList1.push(70);
@@ -12,13 +12,30 @@ public class LinkedListMain {
         linkedList1.push(56);
         linkedList1.display();
         /*
-         UC3: Simple Linked list by Appending 30 and 70 to 56.
+         UC3: Linked list by Appending 30 and 70 to 56.
          */
         LinkedList<Integer> linkedList2 = new LinkedList<>();
         linkedList2.append(56);
         linkedList2.append(30);
         linkedList2.append(70);
         linkedList2.display();
+        /*
+         UC4: Linked list by Appending 30 and 70 to 56.
+         */
+        LinkedList<Integer> linkedList3 = new LinkedList<>();
+        linkedList3.append(56);
+        linkedList3.append(70);
+        linkedList3.display();
+        /*
+         UC5: Linked list Inserting the element 30.
+         */
+        boolean isInserted = linkedList3.insertAfter(56,30);
+        if (isInserted)
+            System.out.println("Element 30 is Inserted");
+        else
+            System.out.println("Element is NOt inserted");
+        linkedList3.display();
+
 
     }
 }
