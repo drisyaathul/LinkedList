0cae1c9419f218a14524c8eeefabf76183993b22
drisyaathul <drisyak809@gmail.com>
2023-04-27 23:25:16 +0800

    Added: Linkedlist by appending 56,30,70

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -20,6 +20,21 @@ public class LinkedList <T>{
             head = newNode;
         }
     }
+    public void append(T data) {
+        /*
+         * New Node is created in the Linked list.so the head and tail is new node.
+         * if head is not equal to null, then tail.next will be new node.
+         * And tail is New node.
+         */
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
     public void display() {
         /*
         Display the linked list
@@ -31,4 +46,6 @@ public class LinkedList <T>{
         }
         System.out.println();
     }
+
+
 }
