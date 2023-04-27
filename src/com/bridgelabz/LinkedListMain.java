1951217341f181c8a027c158ca7d74e407c9ab8b
drisyaathul <drisyak809@gmail.com>
2023-04-27 22:45:37 +0800

    Added: Linkedlist by pushing 70,30,56

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -4,12 +4,12 @@ public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to LinkedList");
         /*
-         UC1: Simple Linked list of 56,30,70.
+         UC2: Simple Linked list by pushing 70,30,56.
          */
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.push(56);
-        linkedList.push(30);
-        linkedList.push(70);
-        linkedList.display();
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+        linkedList1.push(70);
+        linkedList1.push(30);
+        linkedList1.push(56);
+        linkedList1.display();
     }
 }
