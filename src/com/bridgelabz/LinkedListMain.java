8185cd6dca35e1783d195c52b007b7d53f3c896f
drisyaathul <drisyak809@gmail.com>
2023-04-28 22:21:50 +0800

    Added: Linkedlist in Ascending order

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -102,5 +102,19 @@ public class LinkedListMain {
         linkedList5.display();
         linkedList5.size();
         System.out.println("-------------------------------------");
+        /*
+         UC10: Linked list in Ascending Order.
+         */
+        SortedLinkedList sortedLinkedList =  new SortedLinkedList();
+        sortedLinkedList.add(56);
+        sortedLinkedList.add(30);
+        sortedLinkedList.add(40);
+        sortedLinkedList.add(70);
+        System.out.println("Before Sorting :-");
+        sortedLinkedList.display();
+        sortedLinkedList.sortedList();
+        System.out.println("After Sorting :-");
+        sortedLinkedList.display();
+        System.out.println("-------------------------------------");
     }
 }
